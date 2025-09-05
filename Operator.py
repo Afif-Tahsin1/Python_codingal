149259d91911e64bac92859390828a7e6715a944
hhh <jareen0512@gmail.com>
2025-09-05 16:45:13 +0600

    Python

---
+++ Operator.py
@@ -0,0 +1,21 @@
+num1 = 50
+num2 = 3
+
+print("number1", num1)
+print("number2", num2)
+print("Adition:", num1+num2)
+print("Difference", num1-num2)
+print("Product", num1*num2)
+print("Division", num1/num2)
+print("Fllor Division", num1//num2)
+print("Modulus Operator", num1%num2)
+print("Square:", num1**2)
+print("Square root:", num1**0.5)
+
+print("Equal ?", num1==num2)
+print("Number1 greater?", num1>num2)
+print("Number2 greater?", num2>num1)
+print("Not Equal?", num1!=num2)
+
+result = num1/num2+num2**2+10
+print("Result of givven expression is: ", result)
---
+++ datatype.py
@@ -0,0 +1,12 @@
+name = "Penguin" #Str
+age = 15 #int
+is_Student = True #bool
+weight = 38.5 #Float
+
+print("\n After Typoe Casting...")
+age = str(age)
+print(age)
+print("Datatype of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Datatype of weight is", type(weight))

1cc5d5dad09e6ec51fee9446ca270a95ac9aec7d
hhh <jareen0512@gmail.com>
2025-09-06 13:18:02 +0600

    ACP

---
+++ ACP.py
@@ -0,0 +1,15 @@
+first_name = "Python"
+last_name = "Programing"
+full_name = first_name + last_name
+example = "Yess"*5
+
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Full name:", full_name)
+print("String multiplied 5times gives the results:", example)
+
+word = "Coding"
+print("Length of string:", len(word))
+print("First letter of the word:", word[0])
+print("Last letter of the word:", word[5])
+print("String sliced:", word[0:3])
